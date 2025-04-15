52c48d24023a252175953809656e0d3671921c70
Kolby <kolby151@gmail.com>
2025-04-15 20:25:49 -0230

    Added app.route for games.html

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("layout.html")
+    return render_template("index.html")
+
+@app.route("/games")
+def view_games():
+    return render_template("games.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
