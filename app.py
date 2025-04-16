ec0d6a0cb51f076d438e98c55854ce5202f651ec
Kolby <kolby151@gmail.com>
2025-04-16 14:17:46 -0230

    Updated Upload Route to handle post

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, url_for
+import os
 
 app = Flask(__name__)
+app.secret_key = "super_secret_key"
 
 @app.route("/")
 def home():
@@ -10,9 +12,30 @@ def home():
 def view_games():
     return render_template("games.html")
 
-@app.route("/upload")
+@app.route("/upload", methods=["GET", "POST"])
 def upload_game():
-    return render_template("upload.html")
+    if request.method == "GET":
+        return render_template("upload.html")
+
+    elif request.method == "POST":
+        if "games" not in session:
+            session["games"] = []
+
+        title = request.form["title"]
+        platform = request.form["platform"]
+        description = request.form["description"]
+
+        game = {
+            "title": title,
+            "platform": platform,
+            "description": description
+        }
+
+        session["games"].append(game)
+        session.modified = True
+
+        return redirect(url_for("view_games"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
